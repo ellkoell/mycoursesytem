dad311b35ab97e81de971ef8981d34b218dc1226
ellkoell <ellkoell@tsn.at>
2024-12-05 14:36:12 +0100

    MyCourseRepository implementiert

--- src/main/java/dataaccess/MyCourseRepository.java
+++ src/main/java/dataaccess/MyCourseRepository.java
@@ -1,4 +1,16 @@
 package dataaccess;
 
-public interface MyCourseRepository {
+import domain.Course;
+import domain.CourseType;
+
+import java.util.Date;
+import java.util.List;
+
+public interface MyCourseRepository extends BaseRepository<Course,Long> {
+    List<Course> findByCourseName(String name);
+    List<Course> findAllCoursesByDescription(String description);
+    List<Course> findAllCoursesByCourseNameOrDescription(String searchText);
+    List<Course> findAllCoursesByCourseType(CourseType courseType);
+    List<Course> findAllCoursesByStartDate(Date startDate);
+    List<Course> findAllRunningCourses();
 }
